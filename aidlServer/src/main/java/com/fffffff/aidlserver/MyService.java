4266b640d4c373ceffe586741e5fa162421481e3
duke <duke@zhihu.com>
2023-09-28 11:06:32 +0800

    init

--- aidlServer/src/main/java/com/fffffff/aidlserver/MyService.java
+++ aidlServer/src/main/java/com/fffffff/aidlserver/MyService.java
@@ -4,11 +4,9 @@ import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
 import android.os.RemoteException;
-
 import androidx.annotation.Nullable;
-
 import com.fffffff.aidllib.IMyTestAidlInterface;
-import com.fffffff.aidllib.MyCallback;
+import com.fffffff.aidllib.IMyTestCallback;
 
 /**
  * @Author: duke
@@ -20,12 +18,12 @@ public class MyService extends Service {
     private final IMyTestAidlInterface.Stub binderImpl = new IMyTestAidlInterface.Stub() {
 
         @Override
-        public void searchKeyWord(int i, String s, MyCallback myCallback) throws RemoteException {
-            if (myCallback == null) {
+        public void searchKeyWord(int i, String s, IMyTestCallback iMyTestCallback) throws RemoteException {
+            if (iMyTestCallback == null) {
                 return;
             }
             String result = String.valueOf(i) + "_" + s + "_服务端收到了，返回拼接结果给你！";
-            myCallback.onResult(result);
+            iMyTestCallback.onResult(result);
         }
     };
 
